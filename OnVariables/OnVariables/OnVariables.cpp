2b894290e305fe423ba8c867a4d664d3748d4223
伊维兹奈菲斯 <hanlu1726@163.com>
2026-09-19 15:54:32 +0800

    新增“关于变量”项目

---
+++ OnVariables/OnVariables/OnVariables.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	// 变量的定义
+	// 语法：数据类型 变量名 = 初始值;
+	int a = 10; // 定义一个整型变量a，并初始化为10
+	cout << "a = " << a << endl; // 输出变量a的值
+	system("pause");
+	return 0;
+}
