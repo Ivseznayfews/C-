5961e4d01362097d0c666684de3909405cc432d3
伊维兹奈菲斯 <hanlu1726@163.com>
2026-09-19 17:27:29 +0800

    优化关于变量项目，防止std污染

--- OnVariables/OnVariables/OnVariables.cpp
+++ OnVariables/OnVariables/OnVariables.cpp
@@ -1,11 +1,10 @@
 ﻿#include <iostream>
-using namespace std;
 
 int main() {
 	// 变量的定义
 	// 语法：数据类型 变量名 = 初始值;
 	int a = 10; // 定义一个整型变量a，并初始化为10
-	cout << "a = " << a << endl; // 输出变量a的值
-	system("pause");
+	std::cout << "a = " << a << std::endl; // 输出变量a的值
+	std::system("pause");
 	return 0;
 }
