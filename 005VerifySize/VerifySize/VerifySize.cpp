a12e203607d3fcd4f34789d784a833fc97bf9b12
伊维兹奈菲斯 <hanlu1726@163.com>
2026-09-19 19:12:20 +0800

    新增验证大小项目

---
+++ 005VerifySize/VerifySize/VerifySize.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main() {
+	std::cout << "char: " << sizeof(char) << "字节\n";
+	std::cout << "short: " << sizeof(short) << "字节\n";
+	std::cout << "int: " << sizeof(int) << "字节\n";
+	std::cout << "long long: " << sizeof(long long) << "字节\n";
+	std::cout << "float: " << sizeof(float) << "字节\n";
+	std::cout << "double: " << sizeof(double) << "字节\n";
+	std::cout << "bool: " << sizeof(bool) << "字节\n";
+
+	return 0;
+}
